306004c43ed50ea6a7cf91065d4be498ccd345f2
loopbutinfinite <bestrada8800@gmail.com>
2026-07-03 21:12:28 -0700

    Adjusted the app's main component file to add notes about the use of components, as well as calling in our event handler component to get it to display and test it.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,20 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TestComponent } from './test-component/test-component';
+import { EventHandlerComponent } from './event-handler-component/event-handler-component';
 
+// This is the main component file for the app
+// With Angular, if we wanted to call other components in this page like in Next.js, we need to import the class name of the component we want to use in the imports array. As well as call the component selector name in the template below.
+// Note: In this example, we are doing the HTML here in the component, but normally with a bigger project, you would want to do this in its own dedicated html file, the app.htmlm file.
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  imports: [RouterOutlet, EventHandlerComponent],
+  styleUrl: './app.css',
+  template: `
+    <h1>Practice with Angular</h1>
+    <!-- <app-event-handler-component></app-event-handler-component> -->
+     
+  `,
 })
 export class App {
   protected readonly title = signal('angular-practice');
