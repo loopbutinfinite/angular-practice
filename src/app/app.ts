a5a5a1e0512f3d45238389542d775a4e7962f4d0
loopbutinfinite <bestrada8800@gmail.com>
2026-07-04 17:33:14 -0700

    Created a test component to incorporate CSS styling with Tailwind and calling it in the app's main file to display it on the main page.

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,18 +3,21 @@ import { RouterOutlet } from '@angular/router';
 import { TestComponent } from './test-component/test-component';
 import { EventHandlerComponent } from './event-handler-component/event-handler-component';
 import { EventHandlerButtonComponent } from './event-handler-button-component/event-handler-button-component';
+import { LandingPageComponent } from './landing-page-component/landing-page-component';
 
 // This is the main component file for the app
 // With Angular, if we wanted to call other components in this page like in Next.js, we need to import the class name of the component we want to use in the imports array. As well as call the component selector name in the template below.
 // Note: In this example, we are doing the HTML here in the component, but normally with a bigger project, you would want to do this in its own dedicated html file, the app.htmlm file.
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, EventHandlerComponent, EventHandlerButtonComponent],
+  imports: [RouterOutlet, EventHandlerComponent, EventHandlerButtonComponent, LandingPageComponent],
   styleUrl: './app.css',
   template: `
     <h1>Practice with Angular</h1>
     <!-- <app-event-handler-component></app-event-handler-component> -->
-     <app-event-handler-button-component></app-event-handler-button-component>
+     <!-- <app-event-handler-button-component></app-event-handler-button-component>
+       -->
+     <app-landing-page-component></app-landing-page-component>
 
   `,
 })
