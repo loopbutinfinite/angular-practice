33b5e3a12b4bb35f22ee989e35a08844cf8586cf
loopbutinfinite <bestrada8800@gmail.com>
2026-07-06 20:51:59 -0700

    Changed the template of the app component class file to utilize its dedicated html file rather than doing the html in the component class file.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,5 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { TestComponent } from './test-component/test-component';
 import { EventHandlerComponent } from './event-handler-component/event-handler-component';
 import { EventHandlerButtonComponent } from './event-handler-button-component/event-handler-button-component';
@@ -12,17 +12,9 @@ import { DisplayCardsComponent } from './display-cards-component/display-cards-c
 // Note: In this example, we are doing the HTML here in the component, but normally with a bigger project, you would want to do this in its own dedicated html file, the app.htmlm file.
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, EventHandlerComponent, EventHandlerButtonComponent, LandingPageComponent, IteratingComponent, DisplayCardsComponent],
+  imports: [RouterOutlet, EventHandlerComponent, EventHandlerButtonComponent, LandingPageComponent, IteratingComponent, DisplayCardsComponent, RouterLink],
   styleUrl: './app.css',
-  template: `
-    <h1>Practice with Angular</h1>
-    <app-landing-page-component></app-landing-page-component>
-    <app-event-handler-component></app-event-handler-component>
-     <app-event-handler-button-component></app-event-handler-button-component>
-    <app-iterating-component></app-iterating-component>
-    <app-display-cards-component></app-display-cards-component>
-
-  `,
+  templateUrl: './app.html',
 })
 export class App {
   protected readonly title = signal('angular-practice');
