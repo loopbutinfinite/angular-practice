a07bed661a5c10674b463c487011e5de90a7d493
loopbutinfinite <bestrada8800@gmail.com>
2026-07-07 17:35:28 -0700

    Adjusted some CSS styling and created an about us page component to act as a page for the about us to practice with navigating pages in Angular. Not functional at the moment, issue may be with improper value in the routerLink attribute.

---
+++ src/app/about-us-page/about-us-page.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { NavbarComponent } from '../navbar-component/navbar-component';
+
+@Component({
+  selector: 'app-about-us-page',
+  imports: [NavbarComponent],
+  templateUrl: './about-us-page.html',
+  styleUrl: './about-us-page.css',
+})
+export class AboutUsPage {}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { AboutUsPage } from './about-us-page/about-us-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path:"about-us-page", component: AboutUsPage}
+];
