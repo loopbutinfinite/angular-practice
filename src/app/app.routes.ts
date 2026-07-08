e688f935a87e3707191a3716579e5860df031536
loopbutinfinite <bestrada8800@gmail.com>
2026-07-07 17:44:22 -0700

    Created a contact us page component to act as a single page for the contact us to practice navigating through a multi page project in Angular. At the moment, it is not functional, issue may have to do with the app.route file or syntax issue with routerLink.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import { AboutUsPage } from './about-us-page/about-us-page';
+import { ContactUsPage } from './contact-us-page/contact-us-page';
+import { App } from './app';
 
 export const routes: Routes = [
-    { path:"about-us-page", component: AboutUsPage}
+    { path: "HomePage", component: App },
+    { path:"AboutUsPage", component: AboutUsPage },
+    { path:"ContactUsPage", component: ContactUsPage }
 ];
---
+++ src/app/contact-us-page/contact-us-page.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { NavbarComponent } from '../navbar-component/navbar-component';
+
+@Component({
+  selector: 'app-contact-us-page',
+  imports: [NavbarComponent],
+  templateUrl: './contact-us-page.html',
+  styleUrl: './contact-us-page.css',
+})
+export class ContactUsPage {}
